fb97ecaf6dc8efb23473800d9b50211a4770a781
Mate Kukri <km@mkukri.xyz>
2023-09-17 10:10:08 +0100

    hello: add call to `read_key` to avoid double counting keypresses
    When called from GRUB, hello leaved a stray keypress in the buffer,
    and GRUB also registerd it, adding this call stops that.

--- progs/hello/hello.c
+++ progs/hello/hello.c
@@ -9,10 +9,12 @@ efi_status efiapi efi_main(efi_handle image_handle,
                            efi_system_table *system_table)
 {
     efi_size index;
+    efi_in_key key;
 
     efi_init(image_handle, system_table);
     st->con_out->clear_screen(st->con_out);
     efi_print(L"Hello, World from UEFI!\nPress any key to exit\n");
     bs->wait_for_event(1, &st->con_in->wait_for_key, &index);
+    st->con_in->read_key(st->con_in, &key);
     return EFI_SUCCESS;
 }
